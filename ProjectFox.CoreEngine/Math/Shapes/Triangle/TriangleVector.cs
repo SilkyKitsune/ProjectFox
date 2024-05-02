430a50fcdee51237931b13ca1ba6e648e8204346
Jordan Little <jerd.12345@gmail.com>
2024-05-02 14:54:51 -0500

    move angle methods

--- ProjectFox.CoreEngine/Math/Shapes/Triangle/TriangleVector.cs
+++ ProjectFox.CoreEngine/Math/Shapes/Triangle/TriangleVector.cs
@@ -4,26 +4,6 @@ public partial struct Triangle
 {
     /// <summary> Not Yet Implemented </summary>
     /// <returns> default </returns>
-    public float AngleFromRotationOrigin() => default;
-
-    /// <summary> Not Yet Implemented </summary>
-    /// <returns> default </returns>
-    public float Angle(Vector value) => default;
-
-    /// <summary> Not Yet Implemented </summary>
-    /// <returns> default </returns>
-    public float Angle(VectorF value) => default;
-
-    /// <summary> Not Yet Implemented </summary>
-    /// <returns> default </returns>
-    public float Angle(Vector a, Vector b) => default;
-
-    /// <summary> Not Yet Implemented </summary>
-    /// <returns> default </returns>
-    public float Angle(VectorF a, VectorF b) => default;
-
-    /// <summary> Not Yet Implemented </summary>
-    /// <returns> default </returns>
     public Vector.Direction DirectionFromZero() => default;
 
     /// <summary> Not Yet Implemented </summary>
