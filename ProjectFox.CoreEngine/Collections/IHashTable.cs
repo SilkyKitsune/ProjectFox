9ba6b657a1a18280441d6d234b775c2b8bb16777
Jordan Little <jerd.12345@gmail.com>
2024-05-02 15:03:21 -0500

    add methods

--- ProjectFox.CoreEngine/Collections/IHashTable.cs
+++ ProjectFox.CoreEngine/Collections/IHashTable.cs
@@ -14,11 +14,12 @@ public interface IHashTable<H, T>
     public abstract bool Contains(T value);
     public abstract bool ContainsCode(H code);
     public abstract void CopyTo(IHashTable<H, T> table);
+    public abstract H GetCode(T value);
     public abstract H[] GetCodes();
     public abstract T[] GetMultiple(params H[] codes);
     public abstract T[] GetValues();
     public abstract int IndexOf(T value);
-    public abstract int IndexOf(H code);
+    public abstract int IndexOfCode(H code);
     public abstract bool IsEmpty();
     public abstract string Join(char separator);
     public abstract string Join(string separator);
@@ -27,7 +28,9 @@ public interface IHashTable<H, T>
     public abstract string JoinValues(char separator);
     public abstract string JoinValues(string separator);
     public abstract int LastIndexOf(T value);
-    public abstract bool Remove(T value);
     public abstract bool Remove(H code);
     public abstract bool RemoveAt(int index);
+    public abstract bool RemoveValue(T value);
+    public abstract bool TryGet(H code, out T value);
+    public abstract bool TryGetCode(T value, out H code);
 }
