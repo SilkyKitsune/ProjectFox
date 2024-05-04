fd1cb968a4549cf6c4088e525fd32a55f4fd7589
Jordan Little <jerd.12345@gmail.com>
2024-05-04 18:58:52 -0500

    add null checks, fix dpad

--- ProjectFox.GameEngine/Input/Ports.cs
+++ ProjectFox.GameEngine/Input/Ports.cs
@@ -14,83 +14,80 @@ public static class Ports
         {
             InputDevice device = devs.elements[i];
 
-            foreach (DigitalButton button in device.digitalButtons)
-            {
-                bool value = false;
-
-                Array<DigitalButton> bindings = (Array<DigitalButton>)button.bindings;
-                for (int j = 0; j < bindings.length; j++)
-                    value = value || bindings.elements[j].value;
-
-                button.Value = value;//inline?
-            }
-
-            foreach (AnalogButton button in device.analogButtons)
-            {
-                byte value = 0;
-
-                Array<AnalogButton> bindings = (Array<AnalogButton>)button.bindings;
-                for (int j = 0; j < bindings.length; j++)
-                    value = Math.Max(value, bindings.elements[j].value);//is this okay?
-
-                //process these first?
-                Array<DigitalButton> dBindings = (Array<DigitalButton>)button.digitalBindings;
-                for (int j = 0; j < dBindings.length; j++)
-                    if (dBindings[j].value) value = byte.MaxValue;
-
-                button.value = value;
-            }
-
-            foreach (DirectionalPad dPad in device.directionalPads)
-            {
-                Vector.Direction dir = Vector.Direction.Zero;
-
-                Array<DirectionalPad> bindings = (Array<DirectionalPad>)dPad.bindings;
-                for (int j = 0; j < bindings.length; j++)
+            if (device.digitalButtons != null) foreach (DigitalButton button in device.digitalButtons)
                 {
-                    //how to do dpad?
+                    bool value = false;
+
+                    Array<DigitalButton> bindings = (Array<DigitalButton>)button.bindings;
+                    for (int j = 0; j < bindings.length; j++)
+                        value = value || bindings.elements[j].value;
+
+                    button.Value = value;//inline?
                 }
 
-                bool xNeg = false, xPos = false, yNeg = false, yPos = false;
-
-                Array<DigitalButton> xNegBindings = (Array<DigitalButton>)dPad.xNegBindings;
-                for (int j = 0; j < xNegBindings.length; j++)
-                    xNeg = xNeg || xNegBindings.elements[i].value;
-
-                Array<DigitalButton> xPosBindings = (Array<DigitalButton>)dPad.xNegBindings;
-                for (int j = 0; j < xPosBindings.length; j++)
-                    xPos = xPos || xPosBindings.elements[i].value;
-
-                Array<DigitalButton> yNegBindings = (Array<DigitalButton>)dPad.xNegBindings;
-                for (int j = 0; j < yNegBindings.length; j++)
-                    yNeg = yNeg || yNegBindings.elements[i].value;
-
-                Array<DigitalButton> yPosBindings = (Array<DigitalButton>)dPad.xNegBindings;
-                for (int j = 0; j < yPosBindings.length; j++)
-                    yPos = yPos || yPosBindings.elements[i].value;
-
-                Math.Sign x = Math.FindSign(xNeg, xPos), y = Math.FindSign(yNeg, yPos);
-                //combine with dir
-
-                dPad.value = dir;
-            }
-
-            foreach (Stick stick in device.sticks)
-            {
-                Vector pos = new(0, 0);
-
-                Array<Stick> bindings = (Array<Stick>)stick.bindings;
-                for (int j = 0; j < bindings.length; j++)
+            if (device.analogButtons != null) foreach (AnalogButton button in device.analogButtons)
                 {
-                    //how to do analog sticks?
+                    byte value = 0;
+
+                    Array<AnalogButton> bindings = (Array<AnalogButton>)button.bindings;
+                    for (int j = 0; j < bindings.length; j++)
+                        value = Math.Max(value, bindings.elements[j].value);//is this okay?
+
+                    //process these first?
+                    Array<DigitalButton> dBindings = (Array<DigitalButton>)button.digitalBindings;
+                    for (int j = 0; j < dBindings.length; j++)
+                        if (dBindings[j].value) value = byte.MaxValue;
+
+                    button.value = value;
                 }
 
-                stick.Position = pos;//inline?
-            }
+            if (device.directionalPads != null) foreach (DirectionalPad dPad in device.directionalPads)
+                {
+                    Vector.Direction dir = Vector.Direction.Zero;
 
-            foreach (Cursor cursor in device.cursors)
-                if (cursor.binding != null)
-                    cursor.Position = cursor.binding.Position;//inline?
+                    Array<DirectionalPad> bindings = (Array<DirectionalPad>)dPad.bindings;
+                    for (int j = 0; j < bindings.length; j++)
+                    {
+                        //how to do dpad?
+                    }
+
+                    bool xNeg = false, xPos = false, yNeg = false, yPos = false;
+
+                    Array<DigitalButton> xNegBindings = (Array<DigitalButton>)dPad.xNegBindings;
+                    for (int j = 0; j < xNegBindings.length; j++)
+                        xNeg = xNeg || xNegBindings.elements[i].value;
+
+                    Array<DigitalButton> xPosBindings = (Array<DigitalButton>)dPad.xPosBindings;
+                    for (int j = 0; j < xPosBindings.length; j++)
+                        xPos = xPos || xPosBindings.elements[i].value;
+
+                    Array<DigitalButton> yNegBindings = (Array<DigitalButton>)dPad.yNegBindings;
+                    for (int j = 0; j < yNegBindings.length; j++)
+                        yNeg = yNeg || yNegBindings.elements[i].value;
+
+                    Array<DigitalButton> yPosBindings = (Array<DigitalButton>)dPad.yPosBindings;
+                    for (int j = 0; j < yPosBindings.length; j++)
+                        yPos = yPos || yPosBindings.elements[i].value;
+
+                    dPad.value = dir | Vector.FindDirection(Math.FindSign(xNeg, xPos), Math.FindSign(yNeg, yPos));//could this cause sign to be 0b11?
+                }
+
+            if (device.sticks != null) foreach (Stick stick in device.sticks)
+                {
+                    Vector pos = new(0, 0);
+
+                    Array<Stick> bindings = (Array<Stick>)stick.bindings;
+                    for (int j = 0; j < bindings.length; j++)
+                    {
+                        //how to do analog sticks?
+                    }
+
+                    stick.Position = pos;//inline?
+                }
+
+            if (device.cursors != null) foreach (Cursor cursor in device.cursors)
+                    if (cursor.binding != null)
+                        cursor.Position = cursor.binding.Position;//inline?
         }
     }
 }
