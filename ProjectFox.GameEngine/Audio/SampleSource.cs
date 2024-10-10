b71944caf63351ecca8c1d598d1532da9b6368d5
Jordan Little <jerd.12345@gmail.com>
2024-10-10 16:20:22 -0500

    added waveShapeIndex/loop

--- ProjectFox.GameEngine/Audio/SampleSource.cs
+++ ProjectFox.GameEngine/Audio/SampleSource.cs
@@ -8,5 +8,38 @@ public class SampleSource : AudioSource
 
     public Sample[] waveShape = null;
 
-    protected override Sample[] GetDrawInfo() => waveShape;
+    public bool loop = false;
+
+    public int waveShapeIndex = 0;
+
+    protected override Sample[] GetDrawInfo()
+    {
+        if (waveShape == null || waveShape.Length == 0) return waveShape;
+
+        if (waveShapeIndex < 0)
+        {
+            Engine.SendError(ErrorCodes.BadIndex, name, nameof(waveShapeIndex));//neg index message?
+            waveShapeIndex = 0;
+        }
+
+        if (loop)
+        {
+            Sample[] output = new Sample[channel.samples.Length];
+            
+            waveShapeIndex = Math.Wrap(waveShapeIndex, 0, waveShape.Length - 1);//inline later
+            
+            for (int i = 0; i < output.Length; i++, waveShapeIndex++)
+                output[i] = waveShape[Math.Wrap(waveShapeIndex, 0, waveShape.Length - 1)];//inline later
+
+            return output;
+        }
+
+        if (waveShapeIndex >= waveShape.Length) return new Sample[0];
+
+        int index = waveShapeIndex, lastIndex = waveShapeIndex + channel.samples.Length;
+
+        waveShapeIndex = lastIndex;
+
+        return lastIndex >= waveShape.Length ? waveShape[index..] : waveShape[index..lastIndex];
+    }
 }
