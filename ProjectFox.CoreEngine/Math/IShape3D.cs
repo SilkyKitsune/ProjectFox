fcd4b3201c11940934a48d3ffb0fb035c93b8861
Jordan Little <jerd.12345@gmail.com>
2024-10-28 20:35:15 -0500

    removed Equals and added IntersectionVolume

--- ProjectFox.CoreEngine/Math/IShape3D.cs
+++ ProjectFox.CoreEngine/Math/IShape3D.cs
@@ -1,60 +1,38 @@
 ﻿#if DEBUG
-using System.Runtime.CompilerServices;
-
 namespace ProjectFox.CoreEngine.Math;
 
 public interface IShape3D<S, V, t, F, B> : IShape<S, V, t, F, B>
 {//add triangle overloads
     public abstract bool Enveloping(VectorZ value);
+
     public abstract bool Enveloping(VectorZF value);
 
     public abstract bool Enveloping(IPolytope<VectorZ, Tetrahedron> shape);
+
     public abstract bool Enveloping(IPolytope<VectorZF, TetrahedronF> shape);
 
-    public bool Equals(IPolytope<VectorZ, Tetrahedron> shape)
-    {
-        /*if (shape is S s) Equals(s);
+    public abstract Rectangle IntersectionVolume(S shape);//should this always be float?
 
-        V[] points = Points;
-        VectorZ[] shapePoints = shape.Points;
+    public abstract RectangleF IntersectionVolume(F shape);//3d rectangles
 
-        if (points.Length != shapePoints.Length) return false;
+    public abstract Rectangle/*IPolytope<VectorZ, Tetrahedron>*/ IntersectionVolume(IPolytope<VectorZ, Tetrahedron> shape);
 
-        for (int i = 0; i < points.Length; i++)
-            if (!points[i].Equals(shapePoints[i])) return false;*/
-        return true;
-    }
-
-    public bool Equals(IPolytope<VectorZF, TetrahedronF> shape)
-    {
-        /*if (shape is F f) Equals(f);
-
-        V[] points = Points;
-        VectorZF[] shapePoints = shape.Points;
-
-        if (points.Length != shapePoints.Length) return false;
-
-        for (int i = 0; i < points.Length; i++)
-            if (!points[i].Equals(shapePoints[i])) return false;*/
-        return true;
-    }
-
-    public abstract IPolytope<VectorZ, Tetrahedron> IntersectionArea(IPolytope<VectorZ, Tetrahedron> shape);
-    public abstract IPolytope<VectorZF, TetrahedronF> IntersectionArea(IPolytope<VectorZF, TetrahedronF> shape);
+    public abstract RectangleF/*IPolytope<VectorZF, TetrahedronF>*/ IntersectionVolume(IPolytope<VectorZF, TetrahedronF> shape);
 
     public abstract bool Intersecting(IPolytope<VectorZ, Tetrahedron> shape);
+
     public abstract bool Intersecting(IPolytope<VectorZF, TetrahedronF> shape);
 
     public abstract bool Overlapping(VectorZ vector);
+
     public abstract bool Overlapping(VectorZF vector);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]//need to update to abstract
-    public virtual bool Overlapping(IPolytope<VectorZ, Tetrahedron> shape) => Equals(shape) || Enveloping(shape);//|| Within(shape);
+    public abstract bool Overlapping(IPolytope<VectorZ, Tetrahedron> shape);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public virtual bool Overlapping(IPolytope<VectorZF, TetrahedronF> shape) => Equals(shape) || Enveloping(shape);//|| Within(shape);
+    public abstract bool Overlapping(IPolytope<VectorZF, TetrahedronF> shape);
 
     public abstract bool Touching(IPolytope<VectorZ, Tetrahedron> shape);
+
     public abstract bool Touching(IPolytope<VectorZF, TetrahedronF> shape);
 
     //public abstract bool Within(IGenericShape<VectorZ> shape);//redundant?
