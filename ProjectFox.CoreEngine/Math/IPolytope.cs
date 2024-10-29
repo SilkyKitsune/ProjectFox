3d4bd1f97e7623deb8d0e0015ee7c7a2cea81856
Jordan Little <jerd.12345@gmail.com>
2024-10-28 20:23:55 -0500

    added Equals

--- ProjectFox.CoreEngine/Math/IPolytope.cs
+++ ProjectFox.CoreEngine/Math/IPolytope.cs
@@ -6,10 +6,25 @@
 public interface IPolytope<V, P>
 {
     /// <summary> the vector at the center of the shape </summary>
-    public abstract V CenterPoint { get; }
+    public abstract V CenterPoint { get; }//does this make more sense in ishape?
 
     /// <summary> all of the vectors that make up the shape </summary>
     public abstract V[] Points { get; }
+
+    public bool Equals(IPolytope<V, P> shape)//test
+    {
+        //if (shape is S s) Equals(s);
+
+        V[] points = Points, shapePoints = shape.Points;
+
+        if (points.Length != shapePoints.Length) return false;
+
+        for (int i = 0; i < points.Length; i++)
+            if (!points[i].Equals(shapePoints[i])) return false;
+
+        return true;
+    }
+    //public bool Equals(IPolytope<Vf, Pf> shape)//how to do this?
 
     public abstract P[] GetPrimitives();//virtual? can't be virtual because p is undefined
     /*
