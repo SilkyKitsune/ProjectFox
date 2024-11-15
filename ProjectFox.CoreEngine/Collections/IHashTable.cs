c27895fbe5acbe1fc7da2f3fabde2454ead38bb7
Jordan Little <jerd.12345@gmail.com>
2024-11-14 20:19:46 -0600

    renamed IHashTable to ITable

--- ProjectFox.CoreEngine/Collections/IHashTable.cs
+++ ProjectFox.CoreEngine/Collections/IHashTable.cs
@@ -1,25 +1,25 @@
 ﻿namespace ProjectFox.CoreEngine.Collections;
 
-public interface IHashTable<H, T>
+public interface ITable<C, T>
 {
-    public abstract T this[H code] { get; set; }
+    public abstract T this[C code] { get; set; }
 
     public abstract int Length { get; }
 
-    public abstract void Add(H code, T value);
+    public abstract void Add(C code, T value);
     public abstract void Clear();
     public abstract string Concat();
     public abstract string ConcatCodes();
     public abstract string ConcatValues();
     public abstract bool Contains(T value);
-    public abstract bool ContainsCode(H code);
-    public abstract void CopyTo(IHashTable<H, T> table);
-    public abstract H GetCode(T value);
-    public abstract H[] GetCodes();
-    public abstract T[] GetMultiple(params H[] codes);
+    public abstract bool ContainsCode(C code);
+    public abstract void CopyTo(ITable<C, T> table);
+    public abstract C GetCode(T value);
+    public abstract C[] GetCodes();
+    public abstract T[] GetMultiple(params C[] codes);
     public abstract T[] GetValues();
     public abstract int IndexOf(T value);
-    public abstract int IndexOfCode(H code);
+    public abstract int IndexOfCode(C code);
     public abstract bool IsEmpty();
     public abstract string Join(char separator);
     public abstract string Join(string separator);
@@ -28,9 +28,9 @@ public interface IHashTable<H, T>
     public abstract string JoinValues(char separator);
     public abstract string JoinValues(string separator);
     public abstract int LastIndexOf(T value);
-    public abstract bool Remove(H code);
+    public abstract bool Remove(C code);
     public abstract bool RemoveAt(int index);
     public abstract bool RemoveValue(T value);
-    public abstract bool TryGet(H code, out T value);
-    public abstract bool TryGetCode(T value, out H code);
+    public abstract bool TryGet(C code, out T value);
+    public abstract bool TryGetCode(T value, out C code);
 }
