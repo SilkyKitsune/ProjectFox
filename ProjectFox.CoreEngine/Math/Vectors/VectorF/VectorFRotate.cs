2871cb9cdee446269044295e2a10dc2dc1012574
Jordan Little <jerd.12345@gmail.com>
2025-01-06 19:09:27 -0600

    Implemented Angle/AngleFromRotationOrigin

--- ProjectFox.CoreEngine/Math/Vectors/VectorF/VectorFRotate.cs
+++ ProjectFox.CoreEngine/Math/Vectors/VectorF/VectorFRotate.cs
@@ -1,17 +1,67 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
 namespace ProjectFox.CoreEngine.Math;
 
 public partial struct VectorF
 {
-    /// <summary> Not Yet Implemented </summary>
-    /// <returns> default </returns>
-    public static float Angle(VectorF a, VectorF b, VectorF pivot = default) => default;
+    public static float Angle(VectorF a, VectorF b, VectorF pivot = default)
+    {
+        if (a.Equals(b) || a.Equals(pivot) || b.Equals(pivot)) return 0f;
 
-    /// <summary> Not Yet Implemented </summary>
-    /// <returns> default </returns>
-    public float AngleFromRotationOrigin() => default;
+        if (!pivot.IsZero())
+        {
+            a.x -= pivot.x;
+            a.y -= pivot.y;
+            b.x -= pivot.x;
+            b.y -= pivot.y;
+        }
+
+        float a_ = a.AngleFromRotationOrigin(), b_ = b.AngleFromRotationOrigin();
+        return a_ < b_ ? b_ - a_ : a_ - b_;
+    }
+
+    public float AngleFromRotationOrigin()
+    {
+        bool xZero = x == 0, yZero = y == 0;
+
+        if (xZero && yZero) return 0f;
+
+        if (xZero) return y < 0 ? 0f : 0.5f;
+
+        if (yZero) return x < 0 ? 0.75f : 0.25f;
+
+        float angle = 0f;
+        VectorF v;
+
+        int q = (x < 0 ? 0b00 : 0b10) | (y < 0 ? 0b00 : 0b01);
+        switch (q)
+        {
+            case 0b10:
+                v = new(x, -y);
+                break;
+            case 0b11:
+                angle = 0.25f;
+                v = new(y, x);
+                break;
+            case 0b01:
+                angle = 0.5f;
+                v = new(-x, y);
+                break;
+            case 0b00:
+                angle = 0.75f;
+                v = new(-y, -x);
+                break;
+            default:
+                throw new Exception($"Angle error: Quadrant={q}");
+        }
+
+        if (v.x == v.y) return angle + 0.125f;
+
+        return (v.x > v.y ?
+            0.25f - Math.ArcSine(v.y / DistanceFromZero()) :
+            Math.ArcSine(v.x / DistanceFromZero()))
+            + angle;
+    }
 
     public VectorF Rotate(float amount, VectorF pivot = default)
     {
